cc98e9dea405532f0d7175303e1e67fc0b88ead2
Tal <anlv.tan@tendcloud.com>
2015-01-12 13:51:26 +0800

    fix get / save.

--- examples/get_set.js
+++ examples/get_set.js
@@ -2,11 +2,12 @@ var co = require('co'),
     db = require('../').getClient({ servers: ['10.10.35.159:8098'] });
 
 co(function *() {
-    var devices = db.bucket('devices');
+    var devices = db.bucket('dmp', 'devices');
 
-    yield* devices.new('00100', { imei: ['859000000001'], idfa: ['ABCDE'] }).index({ imei: ['859000000001'], idfa: ['ABCDE'] }).save();
-    console.log(yield* devices.get('00100'));
-    console.log(yield* devices.query({ imei: '859000000001' }));
+    yield* devices.new('0010', { imei: ['859000000001'], idfa: ['ABCDE'] }).index({ imei: ['859000000001'], idfa: ['ABCDE'] }).save();
+
+    console.log(yield* devices.get('0010'));
+    console.log(yield* devices.query('imei', '859000000001'));
 }).catch(function (err) {
     console.error(err.stack);
 });
