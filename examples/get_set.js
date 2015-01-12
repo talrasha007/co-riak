bf6b5df3ede8808ff025d36bc1b6d1f8287e91b4
Tal <anlv.tan@tendcloud.com>
2015-01-12 15:30:03 +0800

    parse x-riak-index headers.

--- examples/get_set.js
+++ examples/get_set.js
@@ -4,11 +4,20 @@ var co = require('co'),
 co(function *() {
     var devices = db.bucket('dmp', 'devices');
 
-    yield* devices.new('0010', { imei: ['859000000001'], idfa: ['ABCDE'] }).index({ imei: ['859000000001'], idfa: ['ABCDE', 'FDE'] }).save();
+    yield* devices.new('0010', { imei: ['859000000001'], idfa: ['ABCDE'] }).index({ foo: [1, 2], imei: ['859000000001'], idfa: ['ABCDE', 'FDE'] }).save();
 
-    console.log(yield* devices.get('0010'));
     console.log(yield* devices.query('imei', '859000000001'));
     console.log(yield* devices.query('idfa', 'FDE'));
+
+    var item = yield* devices.get('0010');
+    console.log(item.key());
+    console.log(item.val());
+    console.log(item.meta());
+    yield* item.index({ foo: [2, 3] }, true).save();
+    var item = yield* devices.get('0010');
+    console.log(item.key());
+    console.log(item.val());
+    console.log(item.meta());
 }).catch(function (err) {
     console.error(err.stack);
 });
